3009ecb61109b20cbb99b0ec0c9505e9d1676283
Surya Vijayaraghavan <kvsurii@gmail.com>
2016-04-11 09:32:33 -0700

    Hello world commit for project

---
+++ src/js/client.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Router, Route, IndexRoute, hashHistory } from "react-router";
+
+const app = document.getElementById('app');
+
+ReactDOM.render(<h1>Hello World</h1>,app);
