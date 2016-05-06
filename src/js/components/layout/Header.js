a898db8a7fd421a2798f2a39ca479c19ded89dd0
Surya Vijayaraghavan <kvsurii@gmail.com>
2016-05-06 00:50:00 -0700

    Made changes to the Header

--- src/js/components/layout/Header.js
+++ src/js/components/layout/Header.js
@@ -7,10 +7,15 @@ export default class Header extends React.Component {
       position: "absolute",
       zIndex: "100",
       width: "60px",
-      margin: "20px 0px 0px -50px",
+      margin: "34px 0px 0px -50px",
     };
     const headerStyle = {
       margin: "3px 0px 2px 28px",
+      fontSize: "16px",
+    }
+    const headerDetailsStyle = {
+      marginLeft: "28px",
+      fontSize: "12px",
     }
     const pageHeaderStyle = {
       margin: "10px 0 0",
@@ -23,6 +28,7 @@ export default class Header extends React.Component {
         <div class="container">
           <img src="./media/images/logo.jpg" style={logoStyle} alt="Logo"/>
           <h4 style={headerStyle}>National Consumer Disputes  Redressal Commission | Supreme Court of India</h4>
+          <span style={headerDetailsStyle}>The official forum for dealing with consumer grievances at the national level</span>
         </div>
       </header>
     );
